1a8b0779821f4fe4f71cae01c7c37e0e71d27fae
jwad <89310596+jjrhrh@users.noreply.github.com>
2026-05-01 14:44:20 +0300

    Update 2embed.js

--- providers/2embed.js
+++ providers/2embed.js
@@ -1,34 +1,21 @@
-var TWOEMBED_BASE = "https://www.2embed.cc";
-
 function getStreams(tmdbId, mediaType, season, episode) {
-  return __async(this, null, function* () {
-    try {
-      var isMovie = mediaType !== "tv";
-      var url = isMovie
-        ? TWOEMBED_BASE + "/embed/" + tmdbId
-        : TWOEMBED_BASE + "/embedtv/" + tmdbId + "&s=" + season + "&e=" + episode;
+  var isMovie = mediaType !== "tv";
+  var url = isMovie
+    ? "https://www.2embed.cc/embed/" + tmdbId
+    : "https://www.2embed.cc/embedtv/" + tmdbId + "&s=" + season + "&e=" + episode;
 
-      return [{
-        name: "\u26A1 2Embed",
-        title: "2Embed - 2embed.cc",
-        url: url,
-        quality: "Auto",
-        headers: {
-          "User-Agent": "Mozilla/5.0",
-          "Referer": TWOEMBED_BASE + "/"
-        },
-        isM3U8: false,
-        supportsExternalPlayer: true
-      }];
-    } catch (e) {
-      console.error("[2Embed] Error: " + e.message);
-      return [];
-    }
-  });
+  return Promise.resolve([{
+    name: "2Embed",
+    title: "2Embed - 2embed.cc",
+    url: url,
+    quality: "Auto",
+    headers: {
+      "User-Agent": "Mozilla/5.0",
+      "Referer": "https://www.2embed.cc/"
+    },
+    isM3U8: false,
+    supportsExternalPlayer: true
+  }]);
 }
 
-if (typeof module !== "undefined" && module.exports) {
-  module.exports = { getStreams };
-} else {
-  global.getStreams = getStreams;
-}
+module.exports = { getStreams };
