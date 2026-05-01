e515138c2c733ce9cbd73c98bf36521b463682c9
jwad <89310596+jjrhrh@users.noreply.github.com>
2026-05-01 14:48:21 +0300

    Update vidlink.js

--- providers/vidlink.js
+++ providers/vidlink.js
@@ -1,34 +1,21 @@
-var VIDLINK_BASE = "https://vidlink.pro";
-
 function getStreams(tmdbId, mediaType, season, episode) {
-  return __async(this, null, function* () {
-    try {
-      var isMovie = mediaType !== "tv";
-      var url = isMovie
-        ? VIDLINK_BASE + "/movie/" + tmdbId
-        : VIDLINK_BASE + "/tv/" + tmdbId + "/" + season + "/" + episode;
+  var isMovie = mediaType !== "tv";
+  var url = isMovie
+    ? "https://vidlink.pro/movie/" + tmdbId
+    : "https://vidlink.pro/tv/" + tmdbId + "/" + season + "/" + episode;
 
-      return [{
-        name: "\u{1F517} VidLink",
-        title: "VidLink - vidlink.pro",
-        url: url,
-        quality: "Auto",
-        headers: {
-          "User-Agent": "Mozilla/5.0",
-          "Referer": VIDLINK_BASE + "/"
-        },
-        isM3U8: false,
-        supportsExternalPlayer: true
-      }];
-    } catch (e) {
-      console.error("[VidLink] Error: " + e.message);
-      return [];
-    }
-  });
+  return Promise.resolve([{
+    name: "VidLink",
+    title: "VidLink - vidlink.pro",
+    url: url,
+    quality: "Auto",
+    headers: {
+      "User-Agent": "Mozilla/5.0",
+      "Referer": "https://vidlink.pro/"
+    },
+    isM3U8: false,
+    supportsExternalPlayer: true
+  }]);
 }
 
-if (typeof module !== "undefined" && module.exports) {
-  module.exports = { getStreams };
-} else {
-  global.getStreams = getStreams;
-}
+module.exports = { getStreams };
