ff492a4c605323b3c3f42e8edfa6f8a785adab55
jwad <89310596+jjrhrh@users.noreply.github.com>
2026-05-01 14:46:50 +0300

    Update cinemaos.js

--- providers/cinemaos.js
+++ providers/cinemaos.js
@@ -1,34 +1,21 @@
-var CINEMAOS_BASE = "https://cinemaos.live";
-
 function getStreams(tmdbId, mediaType, season, episode) {
-  return __async(this, null, function* () {
-    try {
-      var isMovie = mediaType !== "tv";
-      var url = isMovie
-        ? CINEMAOS_BASE + "/embed/movie/" + tmdbId
-        : CINEMAOS_BASE + "/embed/tv/" + tmdbId + "/" + season + "/" + episode;
+  var isMovie = mediaType !== "tv";
+  var url = isMovie
+    ? "https://cinemaos.live/embed/movie/" + tmdbId
+    : "https://cinemaos.live/embed/tv/" + tmdbId + "/" + season + "/" + episode;
 
-      return [{
-        name: "\u{1F3A5} CinemaOS",
-        title: "CinemaOS - cinemaos.live",
-        url: url,
-        quality: "Auto",
-        headers: {
-          "User-Agent": "Mozilla/5.0",
-          "Referer": CINEMAOS_BASE + "/"
-        },
-        isM3U8: false,
-        supportsExternalPlayer: true
-      }];
-    } catch (e) {
-      console.error("[CinemaOS] Error: " + e.message);
-      return [];
-    }
-  });
+  return Promise.resolve([{
+    name: "CinemaOS",
+    title: "CinemaOS - cinemaos.live",
+    url: url,
+    quality: "Auto",
+    headers: {
+      "User-Agent": "Mozilla/5.0",
+      "Referer": "https://cinemaos.live/"
+    },
+    isM3U8: false,
+    supportsExternalPlayer: true
+  }]);
 }
 
-if (typeof module !== "undefined" && module.exports) {
-  module.exports = { getStreams };
-} else {
-  global.getStreams = getStreams;
-}
+module.exports = { getStreams };
