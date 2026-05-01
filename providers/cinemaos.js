4f706f7b118a3c3fe2e8f7fe6f28566debcf9c61
jwad <89310596+jjrhrh@users.noreply.github.com>
2026-05-01 16:56:26 +0300

    Update cinemaos.js

--- providers/cinemaos.js
+++ providers/cinemaos.js
@@ -14,7 +14,7 @@ function getStreams(tmdbId, mediaType, season, episode) {
       "Referer": "https://cinemaos.live/"
     },
     isM3U8: false,
-    supportsExternalPlayer: true
+    supportsExternalPlayer: false
   }]);
 }
 
