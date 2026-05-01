cc06f86a14dd59250629bd36123f30a1014077ec
jwad <89310596+jjrhrh@users.noreply.github.com>
2026-05-01 14:45:51 +0300

    Update autoembed.js

--- providers/autoembed.js
+++ providers/autoembed.js
@@ -1,34 +1,21 @@
-var AUTOEMBED_BASE = "https://autoembed.cc";
-
 function getStreams(tmdbId, mediaType, season, episode) {
-  return __async(this, null, function* () {
-    try {
-      var isMovie = mediaType !== "tv";
-      var url = isMovie
-        ? AUTOEMBED_BASE + "/embed/movie/" + tmdbId
-        : AUTOEMBED_BASE + "/embed/tv/" + tmdbId + "/" + season + "/" + episode;
+  var isMovie = mediaType !== "tv";
+  var url = isMovie
+    ? "https://autoembed.cc/embed/movie/" + tmdbId
+    : "https://autoembed.cc/embed/tv/" + tmdbId + "/" + season + "/" + episode;
 
-      return [{
-        name: "\u{1F4E6} AutoEmbed",
-        title: "AutoEmbed - 700K+ أفلام",
-        url: url,
-        quality: "Auto",
-        headers: {
-          "User-Agent": "Mozilla/5.0",
-          "Referer": AUTOEMBED_BASE + "/"
-        },
-        isM3U8: false,
-        supportsExternalPlayer: true
-      }];
-    } catch (e) {
-      console.error("[AutoEmbed] Error: " + e.message);
-      return [];
-    }
-  });
+  return Promise.resolve([{
+    name: "AutoEmbed",
+    title: "AutoEmbed - 700K+",
+    url: url,
+    quality: "Auto",
+    headers: {
+      "User-Agent": "Mozilla/5.0",
+      "Referer": "https://autoembed.cc/"
+    },
+    isM3U8: false,
+    supportsExternalPlayer: true
+  }]);
 }
 
-if (typeof module !== "undefined" && module.exports) {
-  module.exports = { getStreams };
-} else {
-  global.getStreams = getStreams;
-}
+module.exports = { getStreams };
